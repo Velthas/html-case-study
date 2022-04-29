740ec4cb1ac9d54b62c319bce288594ed19d8e0f
Velthas <damianobattaglia97@gmail.com>
2022-04-29 02:35:47 +0200

    Formatted with prettier

--- dropdown/index.js
+++ dropdown/index.js
@@ -1,40 +1,40 @@
 const dropdownGenerator = (function () {
-
   function createLogo() {
     const logo = document.createElement('div');
     logo.classList.add('logo');
-    
+
     const logoContent = prompt('What do you wish to have in the logo?');
     logo.textContent = logoContent;
 
-    return logo
+    return logo;
   }
 
   function createSubItems(number) {
-    
     const containerList = document.createElement('ul');
     containerList.classList.add('sub-menu');
-    
-    for(let i = 0; i < number; i++) {
+
+    for (let i = 0; i < number; i++) {
       const subMenuItem = document.createElement('li');
-      subMenuItem.classList.add('subItem')
-      const itemContent = prompt(`What will sub menu item no.${i + 1} contain?`);
+      subMenuItem.classList.add('subItem');
+      const itemContent = prompt(
+        `What will sub menu item no.${i + 1} contain?`
+      );
       subMenuItem.textContent = itemContent;
       containerList.appendChild(subMenuItem);
     }
 
     return containerList;
-
   }
 
   function createMenuItems(number) {
-
     const navLinks = document.createElement('ul');
-    navLinks.classList.add('nav-links')
+    navLinks.classList.add('nav-links');
     let subItemsList;
 
     for (let i = 0; i < number; i++) {
-      const menuTextContent = prompt(`What will menu item no.${i + 1} contain?`);
+      const menuTextContent = prompt(
+        `What will menu item no.${i + 1} contain?`
+      );
 
       const listItem = document.createElement('li');
       listItem.classList.add('menuItem');
@@ -43,55 +43,54 @@ const dropdownGenerator = (function () {
       mainItem.classList.add('parentItem');
       mainItem.textContent = menuTextContent;
 
-      const numberOfChildren = prompt(`How many sub items will menu ${menuTextContent} have?`);
+      const numberOfChildren = prompt(
+        `How many sub items will menu ${menuTextContent} have?`
+      );
       if (Number(numberOfChildren) !== NaN && numberOfChildren > 0) {
-        mainItem.textContent = mainItem.textContent + ' ▼'
+        mainItem.textContent = mainItem.textContent + ' ▼';
       }
 
-      if (Number(numberOfChildren) === NaN) return
+      if (Number(numberOfChildren) === NaN) return;
       else subItemsList = createSubItems(Number(numberOfChildren));
 
       listItem.appendChild(mainItem);
       listItem.appendChild(subItemsList);
 
-      navLinks.appendChild(listItem)
+      navLinks.appendChild(listItem);
+    }
+
+    return navLinks;
   }
 
-  return navLinks;
-}
-
   function addEvents() {
+    const menuItems = document.querySelectorAll('.menuItem');
+    menuItems.forEach((item) => {
+      const menuHeader = item.querySelector('.parentItem');
+      const submenu = item.querySelector('.sub-menu');
 
-      const menuItems = document.querySelectorAll('.menuItem');
-      menuItems.forEach((item) => {
-        const menuHeader = item.querySelector('.parentItem');
-        const submenu = item.querySelector('.sub-menu');
-    
-        menuHeader.addEventListener('mouseenter', function () {
-          submenu.classList.add('visible');
-          menuHeader.classList.add('active');
-        });
-
-        menuHeader.addEventListener('mouseleave', function () {
-          submenu.classList.remove('visible');
-          menuHeader.classList.remove('active');
-        });
-
-        submenu.addEventListener('mouseenter', function () {
-          submenu.classList.add('visible');
-          menuHeader.classList.add('active');
-        });
-
-        submenu.addEventListener('mouseleave', function () {
-          submenu.classList.remove('visible');
-          menuHeader.classList.remove('active');
-        })
+      menuHeader.addEventListener('mouseenter', function () {
+        submenu.classList.add('visible');
+        menuHeader.classList.add('active');
       });
 
+      menuHeader.addEventListener('mouseleave', function () {
+        submenu.classList.remove('visible');
+        menuHeader.classList.remove('active');
+      });
+
+      submenu.addEventListener('mouseenter', function () {
+        submenu.classList.add('visible');
+        menuHeader.classList.add('active');
+      });
+
+      submenu.addEventListener('mouseleave', function () {
+        submenu.classList.remove('visible');
+        menuHeader.classList.remove('active');
+      });
+    });
   }
 
   function createDropdown() {
-
     //Container
     const navBar = document.createElement('nav');
 
@@ -99,11 +98,13 @@ const dropdownGenerator = (function () {
     const logo = createLogo();
 
     //Gets the number of items on the NavBar
-    const numberOfMenus = prompt('How many navbar items do you want? (numeric value)');
+    const numberOfMenus = prompt(
+      'How many navbar items do you want? (numeric value)'
+    );
 
     let menu;
 
-    //Create the sections of the navbar 
+    //Create the sections of the navbar
     if (Number(numberOfMenus) === NaN) return;
     else menu = createMenuItems(Number(numberOfMenus));
 
@@ -112,9 +113,7 @@ const dropdownGenerator = (function () {
     document.querySelector('body').appendChild(navBar);
 
     addEvents();
-
   }
 
   createDropdown();
-
 })();
